a1916973c698722422990f60f02e8d8942a9560b
Hollow <110562637+edvvarrd@users.noreply.github.com>
2024-08-17 22:02:48 +0300

    Exercise 11.9 #5

--- e2e-test/spec.cy.js
+++ e2e-test/spec.cy.js
@@ -6,4 +6,9 @@ describe('Pokedex', function () {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+  it('particular Pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur').click()
+    cy.contains('chlorophyll')
+  })
 })
